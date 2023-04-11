1485cb9c72d8b75b687748072e7ac01c41e5869d
DhivyaMuniraj <98167208+DhivyaMuniraj@users.noreply.github.com>
2023-04-11 21:32:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
+opublic class HelloWorld{
     public static void main(String args[]){
-        System.out.println("HelloWorld");
+        System.out.println("HellloWorld");
     }
 }
